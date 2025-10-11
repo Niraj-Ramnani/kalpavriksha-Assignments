ece1146f2ceb4cc186b826a80e659cad64b8b2ea
Niraj-Ramnani <neerajramnani800@gmail.com>
2025-10-12 00:24:16 +0530

    added two different functions is validMark and isValidRollNumber to get correct input

--- assignment3_student_perfomance_analyzer/assignment3_student_perfomance_analyzer.c
+++ assignment3_student_perfomance_analyzer/assignment3_student_perfomance_analyzer.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #define MAX_STUDENT 100
+#define NUM_SUBJECTS 3
 
 typedef struct
 {
      int rollNumber;
      char name[50];
-     int marks[3];
+     int marks[NUM_SUBJECTS];
 } Student;
 
 void inputStudentDetails(Student students[], int *n);
 void printStudentData(Student students[], int n);
 void sortStudentsByRoll(Student students[], int n);
-int calculateTotal(int subjectMarks[]);
-float calculateAverage(int totalMarks);
+int calculateTotal(int subjectMarks[], int subjectCount);
+float calculateAverage(int totalMarks, int subjectCount);
 char assignGrade(float averageMarks);
 void printPerformanceStar(char grade);
 void printRollNumber(Student students[], int it, int n);
+bool isValidMark(int subjectMarks[], int subjectCount);
+bool isValidRollNumber(Student students[], int count, int rollNumber);
 
 int main()
 {
@@ -37,10 +41,27 @@ int main()
 void inputStudentDetails(Student students[], int *n)
 {
      scanf("%d", n);
+     bool validRoll = false;
+     bool validMarks = false;
+
      for (int i = 0; i < *n; i++)
      {
-          scanf("%d %s %d %d %d", &students[i].rollNumber, students[i].name,
-                &students[i].marks[0], &students[i].marks[1], &students[i].marks[2]);
+          do
+          {
+
+               scanf("%d %s %d %d %d", &students[i].rollNumber, students[i].name,
+                     &students[i].marks[0], &students[i].marks[1], &students[i].marks[2]);
+               validRoll = isValidRollNumber(students, i, students[i].rollNumber);
+               if (!validRoll)
+               {
+                    printf("\n Error: Duplicate roll number! \n");
+               }
+               validMarks = isValidMark(students[i].marks, NUM_SUBJECTS);
+               if (!validMarks)
+               {
+                    printf("\n Error: Marks must be between 0 and 100.\n");
+               }
+          } while (!validMarks || !validRoll);
      }
 }
 
@@ -48,8 +69,8 @@ void printStudentData(Student students[], int n)
 {
      for (int i = 0; i < n; i++)
      {
-          int totalMarks = calculateTotal(students[i].marks);
-          float averageMarks = calculateAverage(totalMarks);
+          int totalMarks = calculateTotal(students[i].marks, NUM_SUBJECTS);
+          float averageMarks = calculateAverage(totalMarks, NUM_SUBJECTS);
           char grade = assignGrade(averageMarks);
 
           printf("\nRoll: %d\n", students[i].rollNumber);
@@ -82,14 +103,21 @@ void sortStudentsByRoll(Student students[], int n)
      }
 }
 
-int calculateTotal(int subjectMarks[])
+int calculateTotal(int subjectMarks[], int subjectCount)
 {
-     return subjectMarks[0] + subjectMarks[1] + subjectMarks[2];
+     int total = 0;
+     for (int i = 0; i < subjectCount; i++)
+     {
+          total += subjectMarks[i];
+     }
+     return total;
 }
 
-float calculateAverage(int totalMarks)
+float calculateAverage(int totalMarks, int subjectCount)
 {
-     return totalMarks / 3.0;
+     if (subjectCount == 0)
+          return 0.0;
+     return (float)totalMarks / subjectCount;
 }
 
 char assignGrade(float averageMarks)
@@ -139,4 +167,28 @@ void printRollNumber(Student students[], int it, int n)
      }
      printf("%d ", students[it].rollNumber);
      printRollNumber(students, it + 1, n);
+}
+
+bool isValidMark(int subjectMarks[], int subjectCount)
+{
+     for (int i = 0; i < subjectCount; i++)
+     {
+          if (subjectMarks[i] < 0 || subjectMarks[i] > 100)
+          {
+               return false;
+          }
+     }
+     return true;
+}
+
+bool isValidRollNumber(Student students[], int count, int rollNumber)
+{
+     for (int i = 0; i < count; i++)
+     {
+          if (students[i].rollNumber == rollNumber)
+          {
+               return false;
+          }
+     }
+     return true;
 }
